778ab12fe8524e9e212d6395feb34eedcaedd3ae
ram200616 <ramboarar492@gmail.com>
2026-05-27 23:19:32 +0200

    Implement backup download button for music data
    Add a download button for backup CSV file if it exists.

--- music.py
+++ music.py
@@ -33,3 +33,11 @@ st.subheader("previous songs")
 db_reader = pd.read_csv(FILE_NAME)
 
 st.dataframe(db_reader, use_container_width=True)
+f os.path.exists(FILE_NAME):
+    with open(FILE_NAME, "rb") as f:
+        st.download_button(
+            label="تحميل البيانات (نسخة احتياطية)",
+            data=f,
+            file_name="music_backup.csv",
+            mime="text/csv"
+        )
